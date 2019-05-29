e2a7ff7a0719284c8d990890c9da5a1e1df70712
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-05-29 20:18:19 -0300

    rsa skeleton

---
+++ rsa.py
@@ -0,0 +1,39 @@
+def gcd(a, b):
+    """
+    Greatest common divisor
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+'''
+Euclid's extended algorithm for finding the multiplicative inverse of two numbers
+'''
+def multiplicative_inverse(e, phi):
+    return
+
+def is_prime(number):
+    """
+    Test to see if a number is prime.
+    """
+    return True
+
+def generate_keypair(p, q):
+    """
+    p - prime number
+    q - another prime number
+    """
+    # you must use d = multiplicative_inverse(e, phi) and gcd(e, phi)
+    return
+
+def encrypt(pk, message):
+    """
+    pk - private key
+    message - plain text to cipher
+    """
+    cipher = ''
+    return cipher
+
+def decrypt(pk, ciphertext):
+    return
+    
