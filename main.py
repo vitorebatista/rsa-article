f5cbd7e08c8ea0ab7d24a2df138f3b4f47795088
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-05-29 19:55:05 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Implementar um programa para criptografia e descriptografia de um arquivo usando o algoritmo RSA.
+Implementar um algoritmo de força bruta para quebra da chave criptográfica e uma heurística
+que permita quebrar chaves um pouco maiores que o algoritmo de força bruta (e.g. Pollard-Rho).
+
+É obrigatória a implementação das seguintes funções:
+    - Geração das chaves pública e privadas, principalmente a verificação de primalidade de um número;
+    - Algoritmo de Euclides Estendido;
+    - Função para criptografar e descriptografar dados de um arquivo;
+    - Algoritmo de força bruta para a fatoração da chave pública nos números primos que a geraram;
+    - Heurística para fatoração de números primos.
+
+Escrever um relatório técnico de até sete páginas no formato de artigo da SBC, o artigo deve conter:
+    - Uma introdução sobre o funcionamento de criptografia de chave pública;
+    - Uma descrição da complexidade das funções implementadas;
+    - Gráficos com os tempos de execução da geração das chaves e dos dois procedimentos de fatoração para inteiros com n-bits.
+"""
