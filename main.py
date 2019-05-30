b6bc90fc8e60ddcf84acc0ad9f2d5b78f0260b62
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-05-29 23:27:09 -0300

    Func to Class

--- main.py
+++ main.py
@@ -16,61 +16,12 @@ Escrever um relatório técnico de até sete páginas no formato de artigo d
     - Gráficos com os tempos de execução da geração das chaves e dos dois procedimentos de fatoração para inteiros com n-bits.
 """
 
-import rsa
-import random
+from rsa import Rsa
 
-prime_number_limit = 100
+rsa = Rsa()
 
-
-
-
-
-
-def generate_prime():
-    # números primos são valores inteiros maiores que 1
-    # divisíveis apenas por 1 e por si mesmos
-
-    # gera número aleatorio entre 2 e prime_number_limit
-    number = random.randint(2,prime_number_limit)
-
-    # se for par, soma 1 para ser ímpar (pares não são primos, exceto 2)
-    if (number % 2) == 0:
-        number += 1
-
-    # enquanto não for primo, soma dois e tenta novamente
-    while not is_prime(number):
-        number += 2
-        
-    return number
-
-
-
-def is_prime(number):
-    """
-    Test to see if a number is prime.
-    """
-
-    is_prime = True 
-
-    # tenta dividir pelos números a partir de 3 até sua metade, andando de 2 em 2
-    for n in range(3, int(number/2)+2, 2):
-        if (number % n) == 0:
-            is_prime = False
-            break
-        
-    return is_prime
-
-
-
-
-
-
-
-
-q = generate_prime()
-p = generate_prime()
-
-
+q = rsa.generate_prime()
+p = rsa.generate_prime()
 n = p * q
 
 print("primeiro primo: %d" % p)
--- rsa.py
+++ rsa.py
@@ -6,51 +6,84 @@ References:
     https://github.com/sybrenstuvel/python-rsa/blob/master/rsa/prime.py
     https://github.com/MatthewCLind/Crypto_Practice/blob/master/RSA_keygen.py
 """
+import random
 
-def gcd(a, b):
-    """
-    Greatest common divisor
-    """
-    while b != 0:
-        a, b = b, a % b
-    return a
+class Rsa:
+    def __init__(self):
+        self.p = 0
+        self.q = 0
 
-'''
-Euclid's extended algorithm for finding the multiplicative inverse of two numbers
-'''
-def multiplicative_inverse(e, phi):
-    return
+    @staticmethod
+    def gcd(a, b):
+        """
+        Greatest common divisor
+        """
+        while b != 0:
+            a, b = b, a % b
+        return a
 
+    def generate_prime(self):
+        # números primos são valores inteiros maiores que 1
+        # divisíveis apenas por 1 e por si mesmos
+        prime_number_limit = 1000
+        # gera número aleatorio entre 2 e prime_number_limit
+        number = random.randint(2,prime_number_limit)
 
-def gcd(a, b):
-    """
-    Greatest common divisor
-    """
-    while b != 0:
-        a, b = b, a % b
-    return a
+        # se for par, soma 1 para ser ímpar (pares não são primos, exceto 2)
+        if (number % 2) == 0:
+            number += 1
 
-
-def generate_keypair(p, q):
-    """
-    p - prime number
-    q - another prime number
-    """
-    # you must use d = multiplicative_inverse(e, phi) and gcd(e, phi)
-
-
-    return
+        # enquanto não for primo, soma dois e tenta novamente
+        while not self.is_prime(number):
+            number += 2
+            
+        return number
 
 
-def encrypt(pk, message):
-    """
-    pk - private key
-    message - plain text to cipher
-    """
-    cipher = ''
-    return cipher
+    @staticmethod
+    def is_prime(number):
+        """
+        Test to see if a number is prime.
+        """
+
+        is_prime = True 
+
+        # tenta dividir pelos números a partir de 3 até sua metade, andando de 2 em 2
+        for n in range(3, int(number/2)+2, 2):
+            if (number % n) == 0:
+                is_prime = False
+                break
+            
+        return is_prime
 
 
-def decrypt(pk, ciphertext):
-    return
-    
+    '''
+    Euclid's extended algorithm for finding the multiplicative inverse of two numbers
+    '''
+    @staticmethod
+    def multiplicative_inverse(e, phi):
+        return
+
+    @staticmethod
+    def generate_keypair(p, q):
+        """
+        p - prime number
+        q - another prime number
+        """
+        # you must use d = multiplicative_inverse(e, phi) and gcd(e, phi)
+
+        return
+
+    @staticmethod
+    def encrypt(pk, message):
+        """
+        pk - private key
+        message - plain text to cipher
+        """
+        cipher = ''
+        return cipher
+
+    @staticmethod
+    def decrypt(pk, ciphertext):
+        return
+        
