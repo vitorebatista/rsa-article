3da25bfa9f9e9906e2c2c08315120c8a350e17cb
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-06-06 00:02:01 -0300

    fix d using find_inverse

--- rsa.py
+++ rsa.py
@@ -13,55 +13,59 @@ References:
 """
 import random
 import math
-from rsaMath import rsaMath
-
-prime_number_limit = 1000
+from generic import find_inverse, is_prime, xgcd, gcd
 
 # TODO: temos que alterar para usar um teste probabilistico ao invés de fatoracao por inteiros (prof. pediu)
+prime_number_limit = 1000
+
 
 class Rsa:
     def __init__(self):
+        self.bits = 3
+        self.p = 0  # primeiro numero primo
+        self.q = 0  # segundo numero primo
+        self.n = 0  # representa o tamanho do conjunto (p*q)
+        self.e = 0  # primo relativo de phi cujo gdc(e,phi) = 1
+        self.phi = 0  # totiente
 
-        self.p = 0 #primeiro numero primo
-        self.q = 0 #segundo numero primo
-        self.n = 0 #representa o tamanho do conjunto (p*q)
-        self.e = 0 #primo relativo de phi cujo gdc(e,phi) = 1
-        self.phi = 0 #totiente
+        self.publicKey = []  # chave publica
+        self.privateKey = []  # chave privada
 
-        self.publicKey = [] #chave publica
-        self.privateKey =[] #chave privada
+    def set_bits(self, bits) -> None:
+        self.bits = bits
+        return
 
-        self.q = 17#self.generate_prime() #17
-        self.p = 41#self.generate_prime(skip=self.q) #41
-
-        self.generate_keypair(self.p, self.q)
-
-
-    def generate_prime(self, limit: int = prime_number_limit, skip: int = 0, ) -> int:
+    def generate_prime(self, limit: int = 0, skip: int = 0) -> int:
         # números primos são valores inteiros maiores que 1
         # divisíveis apenas por 1 e por si mesmos
 
         # define maior limite entre prime_number_limit e parametro limit
-        limit = prime_number_limit if (limit > prime_number_limit) else limit
-
-        # gera número aleatorio entre 2 e prime_number_limit
-        number = random.randint(2, limit)
+        # limit = prime_number_limit if (limit > prime_number_limit) else limit
+        # TODO Verificar se o uso do número de bits é considerado para o tamanho do primo
+        # https://security.stackexchange.com/a/37910
+        if limit > 0:
+            # gera número aleatorio entre 2 e prime_number_limit
+            number = random.randint(1, limit - 1)
+        else:
+            number = random.randint(2 ** (self.bits - 1), 2 ** (self.bits))
 
         # se for par, soma 1 para ser ímpar (pares não são primos, exceto 2)
         if (number % 2) == 0:
             number += 1
 
         # enquanto não for primo, soma dois e tenta novamente
-        while not rsaMath.is_prime(number):
-            number += 2
+        while not is_prime(number): 
+            number = random.randint(1, limit - 1)
+            # TODO verificar o motivo de +2, pq pode ser maior que o limit ocorrendo erro
+            # number += 2
 
         # garante que p e q sejam números diferentes
-        if (skip == number):
-            #print("p é igual, tentando de novo")
-            return self.generate_prime(skip=skip)
-
+        if skip == number:
+            print("p é igual, tentando de novo")
+            number = self.generate_prime(skip=skip)
+        
+        print('number', number)
         return number
-
 
     def generate_keypair(self, p: int, q: int) -> None:
         """
@@ -69,30 +73,31 @@ class Rsa:
         q - outro número primo
         """
 
-        if not (rsaMath.is_prime(p) and rsaMath.is_prime(q)):
-            raise ValueError('p e q devem ser primos para gerar a chave.')
+        if not (is_prime(p) and is_prime(q)):
+            raise ValueError("p e q devem ser primos para gerar a chave.")
         elif p == q:
-            raise ValueError('p e q não podem ser iguais para gerar a chave.')
+            raise ValueError("p e q não podem ser iguais para gerar a chave.")
 
         # 2. calcule n pela equacao n = p*q
         n = p * q
 
         # 3. selecione um inteiro ímpar pequeno 'e' tal que ele seja primo em relacao
         # a phi(n) que, pela equacao é igual a (p-1)*(q-1)
-        phi = (p-1)*(q-1)
-        e = 13#self.generate_prime(limit=phi) #13
+        phi = (p - 1) * (q - 1)
+        e = self.generate_prime(limit=phi)  # 13
 
         # Acha um inteiro "e" em que "e" e "phi" são coprimos
-        while ( rsaMath.gcd(e, phi) != 1):
+        while gcd(e, phi) != 1:
             print("Não é coprimo, tentando de novo.")
             e = self.generate_prime(limit=phi)
 
         self.p, self.q, self.e, self.phi, self.n = p, q, e, phi, n
 
-        self.publicKey = [e,n]
-        
-        return
+        self.publicKey = [e, n]
+        print('phi', phi)
+        print('publicKey', [e, n])
 
+        return
 
     @staticmethod
     def encrypt(pk: list, message: str) -> list:
@@ -104,22 +109,23 @@ class Rsa:
         crypted_message = []
 
         for c in message:
-            ascii_code = ord(c) #codigo ascii do caracter message
-            coded_letter = pow(ascii_code, pk[0], pk[1]) #letra^e mod n
+            ascii_code = ord(c)  # codigo ascii do caracter message
+            coded_letter = pow(ascii_code, pk[0], pk[1])  # letra^e mod n
             crypted_message.append(coded_letter)
 
         return crypted_message
 
-
     def decrypt(self, message: list) -> str:
 
-        euclides_array = rsaMath.xgcd(self.e, self.phi)
-        self.privateKey = [euclides_array[0], self.n]
-        
-        decrypted_message = ''
+        self.d = find_inverse(self.e, self.phi)
+        self.privateKey = [self.d, self.n]
+        print('e / phi', [self.e, self.phi])
+        print('privateKey', self.privateKey)
+
+        decrypted_message = ""
 
         for c in message:
-            decoded_letter = pow(c,self.privateKey[0],self.privateKey[1])
+            decoded_letter = pow(c, self.privateKey[0], self.privateKey[1])
             decoded_letter = str(chr(decoded_letter))
             decrypted_message += decoded_letter
 
@@ -128,7 +134,7 @@ class Rsa:
     @staticmethod
     def brutalForce(coded_message, pk):
 
-        decrypted_message = ''
+        decrypted_message = ""
 
         p = 1
         q = 1
@@ -139,21 +145,20 @@ class Rsa:
         nSqrt = math.sqrt(n)
         nControl = 3
 
-        #dado n tenta descobrir p e q
+        # dado n tenta descobrir p e q
         while nControl <= nSqrt:
             if (n % nControl) == 0:
-                p = n//nControl
+                p = n // nControl
                 q = nControl
                 break
             nControl += 2
-        
-        phi = (p-1)*(q-1)
 
-        euclides_array = rsaMath.xgcd(e,phi)
-        d = euclides_array[0]
+        phi = (p - 1) * (q - 1)
+
+        d = find_inverse(e, phi)
 
         for c in coded_message:
-            decoded_letter = pow(c,d,n)
+            decoded_letter = pow(c, d, n)
             decoded_letter = str(chr(decoded_letter))
             decrypted_message += decoded_letter
 
