30f1f6145e0ef4d4cc374f9be9155b92ead0db44
felipenwelter <felipenwelter@gmail.com>
2019-06-06 23:01:48 -0300

    showing different values

--- prime_test.py
+++ prime_test.py
@@ -10,6 +10,16 @@ def getPrimeList(n):
             sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
     return [2] + [i for i in range(3,n,2) if sieve[i]]
 
+def print_diff(a1,a2):
+    diff = []
+    for i in a1:
+        if a2.count(i) == 0:
+            diff.append(i)
+    for i in a2:
+        if a1.count(i) == 0:
+            if diff.count(i) == 0:
+                diff.append(i)
+    return diff
 
 amout_test = 10000
 p0 = getPrimeList(amout_test)
@@ -19,34 +29,34 @@ start = t()
 for i in range(2,amout_test):
     p1.append(i) if is_prime(i) else None
 print("is_prime", t()-start) # is_prime 0.5039529800415039
-print("-> OK") if np.array_equal(p0,p1) else print("-> Problema")
+print("-> OK") if np.array_equal(p0,p1) else print("-> Problema",print_diff(p0,p1))
 
 
 start = t()
 for i in range(2,amout_test):
     p2.append(i) if is_prime_fermat(i) else None
 print("is_prime_fermat", t()-start) # is_prime_fermat 7.852689981460571
-print("-> OK") if np.array_equal(p0,p2) else print("-> Problema")
+print("-> OK") if np.array_equal(p0,p2) else print("-> Problema",print_diff(p0,p2))
 
 
 start = t()
 for i in range(2,amout_test):
     p3.append(i) if is_prime_fermat_2(i) else None
 print("is_prime_fermat_2", t()-start) # is_prime_fermat_2 0.012823820114135742
-print("-> OK") if np.array_equal(p0,p3) else print("-> Problema")
+print("-> OK") if np.array_equal(p0,p3) else print("-> Problema",print_diff(p0,p3))
 
 
 start = t()
 for i in range(2,amout_test):
     p4.append(i) if is_prime_fermat_3(i) else None
 print("is_prime_fermat_3", t()-start) # is_prime_fermat_3
-print("-> OK") if np.array_equal(p0,p4) else print("-> Problema")
+print("-> OK") if np.array_equal(p0,p4) else print("-> Problema",print_diff(p0,p4))
 
 
 start = t()
 for i in range(2,amout_test):
     p5.append(i) if is_prime_miller(i) else None
 print("is_prime_miller", t()-start) # is_prime_miller 0.15883302688598633
-print("-> OK") if np.array_equal(p0,p5) else print("-> Problema")
+print("-> OK") if np.array_equal(p0,p5) else print("-> Problema",print_diff(p0,p5))
 
 
