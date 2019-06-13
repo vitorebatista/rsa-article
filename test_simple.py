ec9d9b3251a1eb7831b806f33b92c2075bb6f6c6
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-06-13 12:58:53 -0300

    update test simple

--- test_simple.py
+++ test_simple.py
@@ -4,19 +4,19 @@ import time
 
 def simple_test():
     rsa = Rsa()
-    # p = rsa.generate_prime()
-    # q = rsa.generate_prime(limit=p) # 17
-
-    q = 17
-    p = 41
-    rsa.set_bits(4)
+    rsa.set_bits(512)
+    rsa.set_prime_method('miller')
+    p = rsa.generate_prime()
+    q = rsa.generate_prime(limit=p) 
     rsa.generate_keypair(p, q)
-    coded_message = rsa.encrypt(rsa.publicKey, "This is a message 1234 *&6^'/q@1")
-    decoded_message = rsa.decrypt(coded_message)
-    broken_message = rsa.brutalForce(coded_message, rsa.publicKey)
 
-    print(decoded_message)
-    print("broken_message", broken_message)
+    coded_message = rsa.encrypt(rsa.publicKey, "Felipe Nathan Welter e Vitor Emanuel Batista")
+    decoded_message = rsa.decrypt(coded_message)
+    #broken_message = rsa.brutalForce(coded_message, rsa.publicKey)
+
+    print("coded_message:", coded_message)
+    print("decoded_message:", decoded_message)
+    #print("broken_message", broken_message)
 
 
 simple_test()
